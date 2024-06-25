44c2f6d1bd8d82f4d381c517146b86aa54939915
Diarmuid Collins <dcollins@curtisswright.com>
2024-06-25 11:04:58 +0100

    Added continuity info

--- ubm_msgs.lua
+++ ubm_msgs.lua
@@ -1,8 +1,15 @@
 
 ubm401_proto = Proto("ubm401", "UBM401 Payload")
 
-f_ubm_continuity = ProtoField.bool("ubm401.continuity", "Continuity", base.NONE)    
-f_ubm_padding = ProtoField.uint8("ubm401.padding", "Padding", base.DEC)
+local UBM401_CONTINUITY = {
+	[0x0]="Complete Message",
+	[0x1]="Last Fragment",
+	[0x2]="First Fragment",
+	[0x3]="Middle Fragment",
+}
+
+f_ubm_continuity = ProtoField.uint16("ubm401.continuity", "Continuity", base.HEX, UBM401_CONTINUITY, 0x3000)    
+f_ubm_padding = ProtoField.uint16("ubm401.padding", "Padding", base.DEC, nil, 0xC00)
 
 ubm401_proto.fields = {f_ubm_continuity, f_ubm_padding}
 
@@ -21,12 +28,11 @@ function ubm401_proto.dissector(buffer,pinfo,tree)
 
    local hdr_word = getValue(buffer(offset,2))
         
-  cont  = bit32.extract(hdr_word,12,1)
   pad  = bit32.extract(hdr_word,10,2)
         
   
-  subtree:add(f_ubm_continuity,buffer(offset,2), cont)
-  subtree:add(f_ubm_padding,buffer(offset,2), pad)
+  subtree:add(f_ubm_continuity,buffer(offset,2))
+  subtree:add(f_ubm_padding,buffer(offset,2))
   offset = offset + 2
   subtree:add(buffer(offset, buf_len - pad - 2),"Data")
   offset = offset + buf_len - pad - 2
